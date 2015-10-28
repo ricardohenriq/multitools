685f3f0217850ba262f53bc0be44cd0619903898
Ricardo Henrique <ricardo_henrique007@hotmail.com>
2015-10-28 09:52:39 -0200

    Adicinado os códigos javascript das páginas colored-to-black-white.html e images-to-base64.html aos seu respectivos js.

--- assets/js/colored-to-black-white.js
+++ assets/js/colored-to-black-white.js
@@ -1,1 +1,47 @@
+$('#colored-picture-input').change(function(event){
+	document.getElementById('bw-picture').src = URL.createObjectURL(event.target.files[0]);	         
+});
 
+var bwPictureLoad = false;
+$('#bw-picture').load(function(){
+	if(bwPictureLoad === false){
+		bwPictureLoad = true;
+		var imgObj = document.getElementById('bw-picture');
+		if(navigator.appName == 'Microsoft Internet Explorer'){
+			grayscaleImageIE(imgObj);
+		} else {
+			imgObj.src = grayscaleImage(imgObj);
+		} 
+		$('#bw-picture').css('max-width','100%');
+	}
+});
+
+function grayscaleImageIE(imgObj){
+	imgObj.style.filter = 'progid:DXImageTransform.Microsoft.BasicImage(grayScale=1)';
+}
+
+function grayscaleImage(imgObj){
+	var canvas = document.createElement('canvas');
+	var canvasContext = canvas.getContext('2d');
+	
+	var imgW = imgObj.width;
+	var imgH = imgObj.height;
+	canvas.width = imgW;
+	canvas.height = imgH;
+	
+	canvasContext.drawImage(imgObj, 0, 0);
+	var imgPixels = canvasContext.getImageData(0, 0, imgW, imgH);
+	
+	for(var y = 0; y < imgPixels.height; y++){
+		for(var x = 0; x < imgPixels.width; x++){
+			var i = (y * 4) * imgPixels.width + x * 4;
+			var avg = (imgPixels.data[i] + imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3;
+			imgPixels.data[i] = avg; 
+			imgPixels.data[i + 1] = avg; 
+			imgPixels.data[i + 2] = avg;
+		}
+	}
+	
+	canvasContext.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
+	return canvas.toDataURL();
+}
--- assets/js/images-to-base64.js
+++ assets/js/images-to-base64.js
@@ -1,1 +1,11 @@
-
+//http://davidwalsh.name/convert-image-data-uri-javascript
+function readImage(input) {
+    if ( input.files && input.files[0] ) {
+        var FR= new FileReader();
+        FR.onload = function(e) {
+             $('#img').attr( "src", e.target.result );
+             $('#base').text( e.target.result );
+        };       
+        FR.readAsDataURL( input.files[0] );
+    }
+}
